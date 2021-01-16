9a057f416683c8c57648c221a9f668837aff68c0
Alexander Kasyan <a.kasyan@ntechlab.com>
2021-01-16 04:09:44 +0300

    + simple upload

--- testwidget.cpp
+++ testwidget.cpp
@@ -35,8 +35,6 @@ class TestWidgetPrivate
         });
 
         QObject::connect(ui->uploadPushButton, &QAbstractButton::clicked, [this]{
-            ui->uploadPushButton->setEnabled(false);
-
             QStringList targets;
             for (int i = 0 ; i < ui->filesListWidget->model()->rowCount(); ++i)
                 targets << ui->filesListWidget->item(i)->text();
@@ -44,15 +42,29 @@ class TestWidgetPrivate
             uploader->addFilesForUpload(targets);
         });
 
-        QObject::connect(ui->cancelPushButton, &QAbstractButton::clicked, [this]{
-            uploader->cancelUploads();
-            ui->uploadPushButton->setEnabled(true);
-        });
+        QObject::connect(ui->listFilesPushButton, &QAbstractButton::clicked, uploader, &GDriveUploader::getDriveFilesList);
 
         ui->stackedWidget->setCurrentIndex(1);
         QObject::connect(uploader, &GDriveUploader::grantFinished, [this]{
             ui->stackedWidget->setCurrentIndex(0);
+            ui->logTextEdit->setText(ui->logTextEdit->toPlainText() + "ACCESS GRANTED\n");
         });
+
+        QObject::connect(uploader, &GDriveUploader::networkError, [this](QNetworkReply::NetworkError code){
+            ui->logTextEdit->setText(ui->logTextEdit->toPlainText() + QString("\nNetwork error: %1").arg(code));
+        });
+
+        QObject::connect(uploader, &GDriveUploader::fileListReceived, [this](const QStringList& nameList){
+            ui->driveFilesListWidget->clear();
+            for(const QString& name : nameList) {
+                ui->driveFilesListWidget->addItem(name);
+            }
+        });
+
+        QObject::connect(uploader, &GDriveUploader::displayLogMsg, [this](const QString& msg){
+            ui->logTextEdit->setText(ui->logTextEdit->toPlainText() + QString("\n%1\n").arg(msg));
+        });
+
         uploader->grantAuth();
     }
 
